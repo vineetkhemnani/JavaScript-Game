f8cbc9c4f59f97393b8876d9926add1a1fcbfed0
vineetkhemnani <vineetkhemnani09@gmail.com>
2023-01-13 04:05:29 +0530

    frame animations continuos

--- Project-2 ParallaxBackgrounds/thoughtprocess.js
+++ Project-2 ParallaxBackgrounds/thoughtprocess.js
@@ -19,12 +19,21 @@ backgroundLayer4.src = './backgroundLayers/layer-4.png';
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = './backgroundLayers/layer-5.png';
 
+let x = 0;
+let x2 = 2400;
 
 
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT); //clearing previous frames
-    
+    ctx.drawImage(backgroundLayer5, x, 0);
+    ctx.drawImage(backgroundLayer5, x2, 0);
+    if(x < -2400) x = 2400 + x2 - gameSpeed; // +x2 accounts for the previous frame position
+    else x -= gameSpeed; //traversing horizontally using dynamic gameSpeed
+    if(x2 < -2400) x2 = 2400 + x - gameSpeed ; // x2 = 2400 - gameSpeed -> gap becomes smaller until gameSpeed divides x2 completely
+    else x2 -= gameSpeed;
+    // // gaping present b/w frames so cant be used w/o +x2 and +x
+    // // gameSpeed divisible by imagewidth w/o remainder might be a solution(but we need full dynamic scroll speed)
     requestAnimationFrame(animate);
 };
 animate();
