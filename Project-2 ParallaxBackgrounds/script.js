a84166a36a1e61187b6d3bcaf03366174857ef2b
vineetkhemnani <vineetkhemnani09@gmail.com>
2023-01-13 03:48:51 +0530

    frame animations continuos

--- Project-2 ParallaxBackgrounds/script.js
+++ Project-2 ParallaxBackgrounds/script.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 800; //800
 const CANVAS_HEIGHT = canvas.height = 700; //700
-let gameSpeed = 5; //dynamic variable for scroll speed
+let gameSpeed = 15; //dynamic variable for scroll speed
 
 const backgroundLayer1 = new Image();
 //built-in image class constructor that creates an image element
@@ -18,12 +18,17 @@ const backgroundLayer5 = new Image();
 backgroundLayer5.src = './backgroundLayers/layer-5.png';
 
 let x = 0;
+let x2 = 2400;
 
 function animate(){
-    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT); //clearing previous frames
     ctx.drawImage(backgroundLayer4, x, 0);
-    x--;
+    ctx.drawImage(backgroundLayer4, x2, 0);
+    if(x < -2400) x = 2400;
+    else x -= gameSpeed; //traversing horizontally using dynamic gameSpeed
+    if(x2 < -2400) x2 = 2400;
+    else x2 -= gameSpeed;
+    // gaping present b/w frames so cant be used
     requestAnimationFrame(animate);
 };
 animate();
-console.log("go")
