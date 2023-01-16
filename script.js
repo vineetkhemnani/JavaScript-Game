655b3db4fc5b18d0a67e8200c856c8b8492bbe24
vineetkhemnani <vineetkhemnani09@gmail.com>
2023-01-17 03:23:56 +0530

    completed project-4 collision detection

--- script.js
+++ script.js
@@ -18,14 +18,18 @@ class Explosion {
         this.spriteHeight = 179; // height of each frame
         this.width = this.spriteWidth * 0.7; // width of object displayed
         this.height = this.spriteHeight * 0.7; // height of object displayed
-        this.x = x - this.width/2;
-        this.y = y - this.height/2;
+        this.x = x;
+        this.y = y;
         this.image = new Image();
         this.image.src = './collision/boom.png';
         this.frame = 0;
         this.timer = 0;
+        this.angle = Math.random() * 6.2;  // 360degrees = 6.2 radians
+        this.sound = new Audio();
+        this.sound.src = './collision/boom.wav'
     }
     update(){
+        if (this.frame === 0) this.sound.play();
         this.timer++;
         if (this.timer % 10 === 0){
             this.frame++;
@@ -33,22 +37,38 @@ class Explosion {
         // increase frame every 10 frames-> manage speed of animation
     }
     draw(){
+        ctx.save();
+        ctx.translate(this.x, this.y); // set rotation center point
+        ctx.rotate(this.angle);
         // ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
-        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+        // ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, 0 - this.width/2, 0 - this.height/2, this.width, this.height);// already translated to x,y
+        ctx.restore();
     }
 }
 
 window.addEventListener('click', function(e){
     // console.log(e);
     // ctx.fillRect(x, y, width, height);
-    let positionX = e.x - canvasPosition.left;
-    let positionY = e.y - canvasPosition.top;
-    ctx.fillStyle = 'white';
+    // let positionX = e.x - canvasPosition.left;
+    // let positionY = e.y - canvasPosition.top;
+    // ctx.fillStyle = 'white';
     // ctx.fillRect( e.x - canvasPosition.left - 25, e.y - canvasPosition.top - 25, 50, 50);
     // offset e.x and e.y by top and left margin between viewport and canvas
-    explosions.push(new Explosion(positionX,positionY));
+    createAnimation(e);
 });
 
+// window.addEventListener('mousemove', function(e){
+//     console.log(e);
+//     createAnimation(e);
+// });
+
+
+function createAnimation(e){
+    let positionX = e.x - canvasPosition.left;
+    let positionY = e.y - canvasPosition.top;
+    explosions.push(new Explosion(positionX,positionY));
+}
 function animate(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     for (let i = 0; i < explosions.length; i++){
@@ -56,7 +76,7 @@ function animate(){
         explosions[i].draw();
         if (explosions[i].frame > 5){
             explosions.splice(i, 1);
-            i--;
+            i--; //make sure next object is correctly animated after its neighbor removed,adjust index
         }
     }
     requestAnimationFrame(animate);
