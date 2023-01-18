0da1c708e4dcafc8b643a10187173105633192a1
vineetkhemnani <vineetkhemnani09@gmail.com>
2023-01-19 02:06:39 +0530

    hitbox created

--- Project-5 PointandShootGame/script.js
+++ Project-5 PointandShootGame/script.js
@@ -3,7 +3,11 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+const colllisionCanvas = document.getElementById('collisionCanvas');
+const collisionCtx = collisionCanvas.getContext('2d');
+
 let score = 0; // maintain score
+ctx.font = '50px Impact';
 
 let timeToNextRaven = 0; // accumulate milisecond values b/w frames until its reaches interval value and trigger next frame
 let ravenInterval = 500; // value in ms,time at which next raven is triggered using timeToNextRaven
@@ -29,6 +33,9 @@ class Raven {
         this.maxFrame = 4;
         this.timeSinceFlap = 0;
         this.flapInterval = Math.random() * 50 + 50; //adjust cycling of frames speed
+        this.randomColors = [Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255)];
+        // select random rgb color
+        this.color = 'rgb(' + this.randomColors[0] + ',' + this.randomColors[1] + ',' + this.randomColors[2] + ')';
     }
     update(deltaTime){
         if (this.y < 0 || this.y > canvas.height - this.height){
@@ -48,10 +55,27 @@ class Raven {
 
     }
     draw(){
-        ctx.strokeRect(this.x, this.y, this.width, this.height);
+        ctx.fillStyle = this.color; // random color filled
+        ctx.fillRect(this.x, this.y, this.width, this.height);
         ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
     }
 }
+
+function drawScore(){
+    ctx.fillStyle = 'black';
+    ctx.fillText('Score: ' + score, 50, 75);
+    ctx.fillStyle = 'white';
+    ctx.fillText('Score: ' + score, 53, 80); //two fillstyle for shadow effect
+}
+
+window.addEventListener('click', function(e){
+    console.log(e.x, e.y);
+    const detectPixelColor = ctx.getImageData(e.x, e.y, 1, 1); // getImageData(x,y,width,height)
+    //returns an array like object called Uint8ClampedArray(data structure full of unasigned 8-bit integers)
+    console.log(detectPixelColor);
+    // Uint8Clamped(data)->(red, green, blue, alpha/opacity)
+    // canvas only has ravens rest is transparent, rgb due to CSS body
+});
 
 // const raven = new Raven();
 
@@ -72,6 +96,7 @@ function animate(timestamp){
         timeToNextRaven = 0;
         // console.log(ravens);
     };
+    drawScore();
     // array literal-[...name], ...name->spread operator 
     // create new array and add it to ravens array
     [...ravens].forEach(object=> object.update(deltaTime)); // cycle through ravens array and call update method on each of them
