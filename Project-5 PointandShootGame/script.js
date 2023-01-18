5f8e683bb72a55d818223ab5bada146c45a56355
vineetkhemnani <vineetkhemnani09@gmail.com>
2023-01-18 13:15:22 +0530

    ravens disappear in y direction fix

--- Project-5 PointandShootGame/script.js
+++ Project-5 PointandShootGame/script.js
@@ -29,9 +29,9 @@ class Raven {
         this.flapInterval = Math.random() * 50 + 50; //adjust cycling of frames speed
     }
     update(deltaTime){
-        // if (this.y || this.y > canvas.height - this.height){
-        //     this.directionY = this.directionY * -1;
-        // }
+        if (this.y < 0 || this.y > canvas.height - this.height){
+            this.directionY = this.directionY * -1;
+        }
         this.x -= this.directionX;
         this.y += this.directionY;
         if (this.x < this.width) this.markedForDeletion = true;
