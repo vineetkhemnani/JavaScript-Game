c3190252d069e7c7a5955e02e81220abeeec783e
vineetkhemnani <vineetkhemnani09@gmail.com>
2023-02-03 02:44:40 +0530

    project- 6 completed

--- Project-6 EnemyTypes with JavaScript subclasses/script.js
+++ Project-6 EnemyTypes with JavaScript subclasses/script.js
@@ -2,8 +2,8 @@
 window.addEventListener('load', function(){
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = 500;
+canvas.height = 800;
 
 class Game {
     constructor(ctx, width, height){
