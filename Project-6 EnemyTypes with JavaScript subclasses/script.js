65949a7ca578f08bd2a9256b755eb08da9e8601e
vineetkhemnani <vineetkhemnani09@gmail.com>
2023-02-03 02:40:29 +0530

    project- 6 completed and made code modular

--- Project-6 EnemyTypes with JavaScript subclasses/script.js
+++ Project-6 EnemyTypes with JavaScript subclasses/script.js
@@ -2,8 +2,8 @@
 window.addEventListener('load', function(){
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-canvas.width = 500;
-canvas.height = 800;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 class Game {
     constructor(ctx, width, height){
