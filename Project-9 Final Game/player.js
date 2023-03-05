8fa6da6477c17cda539c018a1b5344c053980f88
vineetkhemnani <vineetkhemnani09@gmail.com>
2023-03-05 17:14:32 +0530

    added different player states

--- Project-9 Final Game/player.js
+++ Project-9 Final Game/player.js
@@ -1,3 +1,4 @@
+import { Sitting, Running, Jumping, Falling } from "./playerStates.js";
 export default class Player{
     constructor(game){
         this.game = game;
@@ -7,11 +8,18 @@ export default class Player{
         this.y = this.game.height - this.height - 10;
         this.vy = 0;
         this.image = document.getElementById('player');
+        this.frameX = 0;
+        this.frameY = 0;
         this.speed = 0;
         this.maxSpeed = 10;
         this.weight = 1;
+        this.states = [new Sitting(this), new Running(this), new Jumping(this), new Falling(this)];
+        this.currentState = this.states[0];
+        this.currentState.enter();
     }
     update(input){
+        this.currentState.handleInput(input); 
+        // horizontal movement
         this.x += this.speed;
         if(input.includes('ArrowRight')) this.speed = this.maxSpeed;
         else if(input.includes('ArrowLeft')) this.speed = - this.maxSpeed;
@@ -19,15 +27,19 @@ export default class Player{
         if (this.x < 0) this.x = 0;
         if (this.x > this.game.width - this.width) this.x = this.game.width - this.width;
         // vertical movement
-        if(input.includes('ArrowUp') && this.onGround()) this.vy -= 20;
+        // if(input.includes('ArrowUp') && this.onGround()) this.vy -= 20;
         this.y += this.vy;
         if(!this.onGround()) this.vy+= this.weight;
         else this.vy = 0;
     }
     draw(context){
-        context.drawImage(this.image, 0, 0, this.width, this.height,this.x, this.y ,this.width, this.height);
+        context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height,this.x, this.y ,this.width, this.height);
     }
     onGround(){
         return this.y >= this.game.height - this.height;
+    }
+    setState(state){
+        this.currentState = this.states[state];
+        this.currentState.enter();
     }
 }
