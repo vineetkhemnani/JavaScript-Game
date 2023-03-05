af18c502a03febf3758849260718592a54de05d0
vineetkhemnani <vineetkhemnani09@gmail.com>
2023-03-05 20:33:39 +0530

    flying enemy done

--- Project-9 Final Game/enemies.js
+++ Project-9 Final Game/enemies.js
@@ -9,7 +9,7 @@ class Enemy{
     }
     update(deltaTime){
     // movement
-    this.x -= this.speedX;
+    this.x -= this.speedX + this.game.speed;
     this.y += this.speedY;
     if(this.frameTimer > this.frameInterval) {
         this.frameTimer = 0;
@@ -32,15 +32,19 @@ export class FlyingEnemy extends Enemy {
         this.game = game;
         this.width = 60;
         this.height = 44;
-        this.x = this.game.width;
+        this.x = this.game.width + Math.random() * this.game.width * 0.5;
         this.y = Math.random() * this.game.height * 0.5;
-        this.speedX = 2;
+        this.speedX = Math.random() + 1;
         this.speedY = 0;
         this.maxFrame = 5;
         this.image = document.getElementById('enemy_fly');
+        this.angle = 0;
+        this.va = Math.random() * 0.1 + 0.1;
     }
     update(deltaTime){
         super.update(deltaTime);
+        this.angle += this.va;
+        this.y += Math.sin(this.angle);
     }
 }
 
