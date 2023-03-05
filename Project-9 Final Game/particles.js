28f932e545797deab5e2db2fde2ea724ad8d25fe
vineetkhemnani <vineetkhemnani09@gmail.com>
2023-03-06 01:34:31 +0530

    added fire effect

--- Project-9 Final Game/particles.js
+++ Project-9 Final Game/particles.js
@@ -19,7 +19,7 @@ export class Dust extends Particle {
         this.y = y;
         this.speedX = Math.random();
         this.speedY = Math.random();
-        this.color = 'rgba(0,0,0,0.3 )';
+        this.color = 'rgba(0,0,0,0.3)';
     }
     draw(context){
         context.beginPath();
@@ -34,5 +34,27 @@ export class Splash extends Particle {
 }
 
 export class Fire extends Particle {
-
+    constructor(game, x, y){
+        super(game);
+        this.image = document.getElementById('fire');
+        this.size = Math.random() * 100 + 100;
+        this.x = x;
+        this.y = y;
+        this.speedX = 1;
+        this.speedY = 1;
+        this.angle = 0;
+        this.va = Math.random() * 0.2 - 0.1;
+    }
+    update(){
+        super.update();
+        this.angle += this.va;
+        this.x += Math.sin(this.angle * 5);
+    }
+    draw(context){
+        context.save();
+        context.translate(this.x, this.y);
+        context.rotate(this.angle);
+        context.drawImage(this.image, -this.size * 0.5, 0.5 * -this.size, this.size, this.size);
+        context.restore();
+    }
 }
