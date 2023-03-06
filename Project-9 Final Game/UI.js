a26ac62877edc3a34efde467df98dfdf42c45ed7
vineetkhemnani <vineetkhemnani09@gmail.com>
2023-03-06 12:27:07 +0530

    score UI and timer added

--- Project-9 Final Game/UI.js
+++ Project-9 Final Game/UI.js
@@ -5,10 +5,33 @@ export class UI {
         this.fontFamily = 'Helvetica';
     }
     draw(context){
+        context.save();
+        context.shadowOffsetX = 2;
+        context.shadowOffsetY = 2;
+        context.shadowColor = 'white';
+        context.shadowBlur = 0;
         context.textAlign = 'left';
         context.fillStyle = this.game.fontColor;
         context.font = this.fontSize + 'px ' + this.fontFamily;
-        //score
+        // score
         context.fillText('Score: ' + this.game.score, 20, 50);
+        // timer
+        context.font = this.fontSize * 0.8 + 'px ' + this.fontFamily;
+        context.fillText('Time: ' + (this.game.time * 0.001).toFixed(1), 20, 80);
+        // game over messages
+        if (this.game.gameOver) {
+            context.textAlign = 'center';
+            context.font = this.fontSize * 2 + 'px '+ this.fontFamily;
+            if(this.game.score > 5){
+                context.fillText('Boo-yah', this.game.width * 0.5, this.game.height * 0.5 - 20);
+                context.font = this.fontSize * 0.7 + 'px '+ this.fontFamily;
+                context.fillText('What are creatures of the night afraid of? YOU!!!', this.game.width * 0.5, this.game.height * 0.5 + 20);
+            }else{
+                context.fillText('Love at first bite?', this.game.width * 0.5, this.game.height * 0.5 - 20);
+                context.font = this.fontSize * 0.7 + 'px '+ this.fontFamily;
+                context.fillText('Nope.Better luck next time', this.game.width * 0.5, this.game.height * 0.5 + 20);
+            }
+        }
+        context.restore();
     }
 }
